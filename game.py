7b64ebbf4a4ab35cd70adca6cc690198c8d8bc78
Krestixx <hahdan14@gmail.com>
2025-10-20 23:13:02 +0500

    Update game.py

--- game.py
+++ game.py
@@ -1,19 +1,43 @@
 import random
 
-secret = random.randint(1, 10)
+diff = int(input('Сложность\nЛегко - 1 \nСредне - 2 \nСложно - 3\nВаш выбор:'))
+
+if diff == 1:
+    secret = random.randint(1,10)
+elif diff == 2:
+     secret = random.randint(10,35)
+else:
+    secret = random.randint(25,75)
+
+
 guessed = False
-attemps = 0
+attempts = 0
 while not guessed:
     try:
         number = int(input('Компьютер выбрал число, введи своё: '))
-        attemps += 1
+        attempts += 1
         if number == secret:
-            gambling = int(input(f'Вы угадали число за {attemps} попыток!\n Продолжить - 1 \n Выйти - 2 \n Продолжить?: '))     
+            gambling = int(input(f'Вы угадали число за {attempts} попыток!\n Продолжить - 1 \n Выйти - 2\n Сменить сложность - 3\n Продолжить?: '))     
             if gambling == 2:
-                break
+                break            
             if gambling == 1:
-                secret = random.randint(1,10)
-                attemps = 0           
+                attempts = 0
+                if diff == 1:
+                    secret = random.randint(1,10)
+                elif diff == 2:
+                    secret = random.randint(10,35)
+                else:
+                    secret = random.randint(25,75)
+            elif gambling == 3:
+                attempts = 0
+                diff = int(input('Сложность\nЛегко - 1 \nСредне - 2 \nСложно - 3\nВаш выбор:'))
+                if diff == 1:
+                    secret = random.randint(1,10)
+                elif diff == 2:
+                    secret = random.randint(10,35)
+                else:
+                    secret = random.randint(25,75)
+         
         else:
             if number > secret:
                 print(f'Число заданное компьютером меньше чем: {number}')
@@ -21,5 +45,3 @@ while not guessed:
                 print(f'Число заданное компьютером больше чем: {number}')
     except ValueError:
         print('Ввод принимает только числа')
-
-    
